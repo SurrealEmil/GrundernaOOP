d34fb0fd72718b4065556f92f918e45e514a25bb
SurrealEmil <emil.ejderklev1@hotmail.com>
2023-10-15 15:36:25 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundernaOOP
+{
+    internal class Circle
+    {
+        private int _radius;
+
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+
+        // Three metods getting area, volume and circumfrence.
+        // Using Math.Round that rounds the decimal to the sett amount in this case three.
+        // Using Math.Pi to use pi.
+        // Using Math.Pow insted of example _radius * _radius * _radius. I can use Math.Pow and take _radius to the power of three.
+
+        public double getArea()
+        {
+            return Math.Round(Math.PI * Math.Pow(_radius, 2), 3);
+        }
+
+        public double getVolume()
+        {
+            return Math.Round((4.0 / 3.0) * Math.PI * Math.Pow(_radius, 3), 3);
+        }
+
+        public double getCircumference()
+        {
+            return Math.Round(2 * Math.PI * _radius, 3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            double area1 = circle1.getArea();
+            double area2 = circle2.getArea();
+
+            double volume1 = circle1.getVolume();
+            double volume2 = circle2.getVolume();
+
+            double circum1 = circle1.getCircumference();
+            double circum2 = circle2.getCircumference();
+
+
+            Console.WriteLine($"Arean är {area1}\nVolymen är {volume1}\nOmkretsen är {circum1}\n");
+            Console.WriteLine($"Arean2 är {area2}\nVolymen2 är {volume2}\nOmkretsen2 är {circum2}");
+        }
+    }
+}
